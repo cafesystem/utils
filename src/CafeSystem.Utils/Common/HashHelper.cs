b81222b11791b58a33e285df8da78b9b37ac927c
imun <imun22@gmail.com>
2023-02-07 14:36:33 +0330

    Add HashHlepr to hash strings with an optional salt

---
+++ src/CafeSystem.Utils/Common/HashHelper.cs
@@ -0,0 +1,30 @@
+using System.Security.Cryptography;
+using System.Text;
+
+namespace CafeSystem.Utils
+{
+    
+    public static class HashHelper
+    {
+        
+        /// <summary>
+        /// Hash input string with an optional salt.
+        /// </summary>
+        /// <param name="input"></param>
+        /// <param name="salt"></param>
+        /// <returns></returns>
+        public static string HashString(this string input, string salt = null) {
+            if(salt.IsNullOrEmpty()) input = $"{input}{salt}";
+
+            using (var md5Hash = MD5.Create())
+            {
+                byte[] data = md5Hash.ComputeHash(Encoding.UTF8.GetBytes(input));
+                StringBuilder sBuilder = new StringBuilder();
+                for (int i = 0; i < data.Length; i++)
+                {
+                    sBuilder.Append(i.ToString("x2"));
+                }
+                return sBuilder.ToString();
+            }
+        }    }
+}
