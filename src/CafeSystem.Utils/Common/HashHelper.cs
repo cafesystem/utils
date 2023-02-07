7307fdfebbfae3fdb591403b2a14ab81eb05fac9
imun <imun22@gmail.com>
2023-02-07 16:05:39 +0330

    Add Md5Hash method

--- src/CafeSystem.Utils/Common/HashHelper.cs
+++ src/CafeSystem.Utils/Common/HashHelper.cs
@@ -1,20 +1,19 @@
-using System.Security.Cryptography;
+using System;
 using System.Text;
+using System.Security.Cryptography;
 
 namespace CafeSystem.Utils
 {
-    
     public static class HashHelper
     {
-        
         /// <summary>
         /// Hash input string with an optional salt.
         /// </summary>
         /// <param name="input"></param>
         /// <param name="salt"></param>
-        /// <returns></returns>
-        public static string HashString(this string input, string salt = null) {
-            if(salt.IsNullOrEmpty()) input = $"{input}{salt}";
+        public static string HashString(this string input, string salt = null)
+        {
+            if (salt.IsNullOrEmpty()) input = $"{input}{salt}";
 
             using (var md5Hash = MD5.Create())
             {
@@ -24,7 +23,22 @@ namespace CafeSystem.Utils
                 {
                     sBuilder.Append(i.ToString("x2"));
                 }
+
                 return sBuilder.ToString();
             }
-        }    }
+        }
+
+
+        /// <summary>
+        /// Returns MD5 hash for a given string.
+        /// </summary>
+        /// <param name="input">The string which you want to MD5</param>
+        public static string Md5Hash(this string input)
+        {
+            MD5 md5 = new MD5CryptoServiceProvider();
+            var encodedBytes = md5.ComputeHash(Encoding.Default.GetBytes(input));
+            return BitConverter.ToString(encodedBytes);
+        }
+        
+    }
 }
